2ece26c160f1768413b8eed3e21fdf02551301b7
Elisa Schösser <eschoe@mpe.mpg.de>
2023-11-21 16:34:35 +0100

    logging in init

--- cosmogrb/__init__.py
+++ cosmogrb/__init__.py
@@ -9,3 +9,9 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from cosmogrb.utils.logging import (
+    activate_warnings,
+    silence_warnings,
+    update_logging_level,
+)
